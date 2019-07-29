98fd4972fbbfdaa58ec6db5d1176bb45cc03f57c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-07-29 16:53:05 +0700

    Create UI List Songs

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,254 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF7D9AFF),
+        accentColor: Color(0xFF7D9AFF),
+      ),
       home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  List<Song> listSong = List();
+
   @override
   Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    initListSong();
+
     return Scaffold(
-      body: Center(
-        child: Text('hello world'),
+      body: Stack(
+        children: <Widget>[
+          _buildWidgetAlbumCover(mediaQuery),
+          _buildWidgetActionAppBar(mediaQuery),
+          _buildWidgetArtistName(mediaQuery),
+          _buildWidgetFloatingActionButton(mediaQuery),
+          _buildWidgetListSong(mediaQuery),
+        ],
       ),
     );
+  }
+
+  Widget _buildWidgetArtistName(MediaQueryData mediaQuery) {
+    return SizedBox(
+      height: mediaQuery.size.height / 1.8,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Stack(
+              children: <Widget>[
+                Positioned(
+                  child: Text(
+                    "Grande",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "CoralPen",
+                      fontSize: 72.0,
+                    ),
+                  ),
+                  top: constraints.maxHeight - 100.0,
+                ),
+                Positioned(
+                  child: Text(
+                    "Ariana",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "CoralPen",
+                      fontSize: 72.0,
+                    ),
+                  ),
+                  top: constraints.maxHeight - 140.0,
+                ),
+                Positioned(
+                  child: Text(
+                    "Tranding",
+                    style: TextStyle(
+                      color: Color(0xFF7D9AFF),
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  top: constraints.maxHeight - 160.0,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetListSong(MediaQueryData mediaQuery) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 20.0,
+        top: mediaQuery.size.height / 1.8 + 48.0,
+        right: 20.0,
+        bottom: mediaQuery.padding.bottom + 16.0,
+      ),
+      child: Column(
+        children: <Widget>[
+          _buildWidgetHeaderSong(),
+          SizedBox(height: 16.0),
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.zero,
+              separatorBuilder: (BuildContext context, int index) {
+                return Opacity(
+                  opacity: 0.5,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 2.0),
+                    child: Divider(
+                      color: Colors.grey,
+                    ),
+                  ),
+                );
+              },
+              itemCount: listSong.length,
+              itemBuilder: (BuildContext context, int index) {
+                Song song = listSong[index];
+                return Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        song.title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Text(
+                      song.duration,
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(width: 24.0),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.grey,
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWidgetHeaderSong() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "Popular",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+            fontSize: 24.0,
+          ),
+        ),
+        Text(
+          "Show all",
+          style: TextStyle(
+            color: Color(0xFF7D9AFF),
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWidgetFloatingActionButton(MediaQueryData mediaQuery) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: mediaQuery.size.height / 1.8 - 32.0,
+          right: 32.0,
+        ),
+        child: FloatingActionButton(
+          child: Icon(
+            Icons.play_arrow,
+            color: Colors.white,
+          ),
+          backgroundColor: Color(0xFF7D9AFF),
+          onPressed: () {
+            // TODO: do something in here
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetActionAppBar(MediaQueryData mediaQuery) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 16.0,
+        top: mediaQuery.padding.top + 16.0,
+        right: 16.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.info_outline,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWidgetAlbumCover(MediaQueryData mediaQuery) {
+    return Container(
+      width: double.infinity,
+      height: mediaQuery.size.height / 1.8,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(48.0),
+        ),
+        image: DecorationImage(
+          image:
+              AssetImage("assets/ariana_grande_cover_no_tears_left_to_cry.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  void initListSong() {
+    listSong.add(Song(title: "No tears left to cry", duration: "5:20"));
+    listSong.add(Song(title: "Imagine", duration: "3:20"));
+    listSong.add(Song(title: "Into you", duration: "4:12"));
+    listSong.add(Song(title: "One last time", duration: "4:40"));
+    listSong.add(Song(title: "7 rings", duration: "2:58"));
+    listSong.add(Song(title: "Thank u, next", duration: "3:27"));
+    listSong.add(Song(
+        title: "Break up with your girlfriend, i'm bored", duration: "3:10"));
+    listSong.add(Song(title: "Test", duration: "0:00"));
+  }
+}
+
+class Song {
+  String title;
+  String duration;
+
+  Song({this.title, this.duration});
+
+  @override
+  String toString() {
+    return 'Song{title: $title, duration: $duration}';
   }
 }
